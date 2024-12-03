c462274312a45fbf1e9139c7b48d5eb4f353d530
Gabriel Morales <gabrielmorales1@protonmail.com>
2024-12-03 16:22:34 -0500

    build: base dockerization

--- src/main.py
+++ src/main.py
@@ -15,10 +15,4 @@ app = FastAPI(
     version="0.1.0",
 )
 
-
 app.include_router(router=healthcheck_router)
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
