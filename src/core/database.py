0658f9ae0a7b71403ea8bfc77e23b5b3387e67c9
Gabriel Morales <gabrielmorales1@protonmail.com>
2024-12-08 09:01:35 -0500

    refactor(database): remove unused get_db function

--- src/core/database.py
+++ src/core/database.py
@@ -1,17 +1,5 @@
-from collections.abc import Generator
-from typing import Annotated
-
-from fastapi import Depends
-from sqlmodel import Session, create_engine
+from sqlmodel import create_engine
 
 from src.core.settings import settings
 
 engine = create_engine(url=settings.POSTGRES_URL)  # type: ignore[arg-type]
-
-
-def get_db() -> Generator[Session, None, None]:
-    with Session(engine) as session:
-        yield session
-
-
-SessionDep = Annotated[Session, Depends(get_db)]
