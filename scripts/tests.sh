21d5fd6f2660694b0030fc3ff27c7f26be2a92f1
Gabriel Morales <gabrielmorales1@protonmail.com>
2024-12-08 22:32:43 -0500

    feat(scripts): update tests script

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -2,5 +2,6 @@
 
 set -o errexit
 
-poetry install --no-root --no-interaction --no-ansi --with=dev
-poetry run pytest
+docker compose -f docker-compose.dev.yml build
+docker compose -f docker-compose.dev.yml up -d
+docker compose -f docker-compose.dev.yml exec mis-eventos-app pytest --cov=src
